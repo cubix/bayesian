50fac05622171d9595a722766188dfcb302d1220
Cubix <cubix@pobox.com>
2010-03-26 15:24:37 -0400

    put memoization in a binding

--- bayesian.clj
+++ bayesian.clj
@@ -10,23 +10,25 @@
 	(recur (conj acc {elt n}) next 1 (rest lst)))))) 
 
 (defn compress
-  "Run length encoding. (Based the version in book ANSI Common Lisp.)"
+  "Run length encoding. (Based on version in book ANSI Common Lisp.)"
   [x]
   (vec (compr [] (first x) 1 (rest x)))) 
 
 (defn word-count
-  "Creates a count of words by sorting and then compressing it."
+  "Creates a hash-map of word counts."
   [lst]
   (apply merge (-> lst sort compress)))
 
-(defn parse-file [file]
+(defn parse-file
+  "Extracts words from file for counting."
+  [file]
   (word-count 
    (filter #(re-matches #"[a-z]{3,44}" %)
 	   (map (memfn toLowerCase)
 		(re-split #"\s+" 
 			  (slurp file))))))
 
-(def words (ref (load-file "words.clj")))
+(def words (ref (load-file "/tmp/words.clj")))
 
 (defn update-category [cat wc]
   (dosync (alter words assoc-in [cat] wc)))
@@ -40,13 +42,11 @@
   [db cat]
   (let [wc (db cat)]
    (reduce + (map (fn [[word c]] c) wc))))
-(def category-count (memoize category-count))
 
 (defn total-count
   "Total words across all categories."
   [db]
   (reduce + (map #(category-count db %)  (keys db))))
-(def total-count (memoize total-count))
 
 (defn cat-word [db cat word]
   ((db cat) word))
@@ -62,27 +62,28 @@
 (defn classify
   "Get the classification of a file from word counts"
   [db words-in-file]
-  (let [categories (keys db)
-	word-list (keys words-in-file)]
-    (apply merge-with +
-	   (concat 
-	    ; Run through words and calculate the probability for each category
-	    (map (fn [word] 
-		   (apply merge (map (fn [cat] 
-				       (hash-map cat (word-score db cat word)))
-					    categories))) 
-		 word-list)
-	    ; Add in the probability that the text is of a specific category
-	    (map (fn [cat] (hash-map cat (category-score db cat)))
-		 categories)))))
-
+  (binding [total-count (memoize total-count)
+	    category-count (memoize category-count)]
+    (let [categories (keys db)
+	 word-list (keys words-in-file)]
+     (println (apply merge-with +
+	     (concat 
+	      ;; Run through words and calculate the probability for each category
+	      (map (fn [word] 
+		     (apply merge (map (fn [cat] 
+					 (hash-map cat (word-score db cat word)))
+				       categories))) 
+		   word-list)
+	      ;; Add in the probability that the text is of a specific category
+	      (map (fn [cat] (hash-map cat (category-score db cat)))
+		   categories)))))))
 (defn main []
   (let [cmd (first *command-line-args*)]
     (cond
 	(= cmd "add")
  	 (do (add-words (second *command-line-args*) 
 			(parse-file (nth *command-line-args* 2)))
-	     (spit "words.clj" @words))
+	     (spit "/tmp/words.clj" @words))
         (= cmd "classify")
 	 (classify @words (parse-file (second *command-line-args*))))))
 
